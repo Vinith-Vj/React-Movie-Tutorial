a2cd3b89f50cb092e30017e2a76e81f15670d606
Vinith-Vj <vinithvj555@gmail.com>
2025-04-10 15:41:29 +0530

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "8406c1092487ac7f5d034b5a2eeb334e";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
